04d25426302c4eef3fc18d2b499655a02cf94097
STEVE LEONEL YOMI MBIAKOP <leonelyomi3@gmail.com>
2024-12-14 04:03:06 +0100

    springboot intialisation

---
+++ bookingssystem/src/main/java/steve/bookingssystem/BookingssystemApplication.java
@@ -0,0 +1,13 @@
+package steve.bookingssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingssystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingssystemApplication.class, args);
+	}
+
+}
---
+++ bookingssystem/src/test/java/steve/bookingssystem/BookingssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package steve.bookingssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingssystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
